e3aad6b4c8b0658ca3d16eb71ee99f3051b2705b
Nicholas Williams <nawilliams@umass.edu>
2017-11-04 19:19:14 -0400

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'scanner/index'
+  root 'scanner#index'
 
-  get 'scanner/report'
+  get '/report', to: 'scanner#report'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
