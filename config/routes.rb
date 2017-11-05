3672d1603cac17c98e919e433c991e09036d6e1f
Nicholas Williams <nawilliams@umass.edu>
2017-11-05 02:00:24 -0500

    scan path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'scanner#index'
 
   get '/report', to: 'scanner#report'
+  post '/scan', to: 'scanner#scan'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
