e2b5f55f7472e48bf7dae4ac0930d3bcb0941545
Cobchise <ciaranobrien@umass.edu>
2017-11-05 01:54:48 -0500

    Centered form

--- app/controllers/scanner_controller.rb
+++ app/controllers/scanner_controller.rb
@@ -1,9 +1,15 @@
 class ScannerController < ApplicationController
   def index
-    @options = ['a']
+    @options = Hash.new
+    @options['Discovery'] = "-sn"
+    @options['Full'] = "-sV --version-intensity 8"
   end
 
   def report
     @ip = params[:ip_address]
+  end
+
+  def scan
+    
   end
 end
